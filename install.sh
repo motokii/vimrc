5d72e9eabf6dbbf001989fc349af9fdf734832af
motoki nagaoka <motoki.23ar@gmail.com>
2016-01-22 17:23:14 +0900

    Rename .vimrc

--- install.sh
+++ install.sh
@@ -2,12 +2,11 @@
 check_file() {
     if [ -e "$1" ]; then
         echo "Error, $1 already exists."
+        echo "Please rename or remove $1."
         exit
-    else
-        echo "$1 not exists"
     fi
 }
 check_file "$HOME/.vimrc"
 check_file "$HOME/.vim/vimrc"
 git clone https://github.com/motokii/vimrc.git $HOME/.vim/vimrc
-ln -s $HOME/.vim/vimrc/.vimrc $HOME/.vimrc
+ln -s $HOME/.vim/vimrc/.vimrc $HOME/vimrc
